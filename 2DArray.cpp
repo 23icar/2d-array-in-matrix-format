4409f7f381fbfbb1097b82432ff9b36258e91f5c
23icar <108954738+23icar@users.noreply.github.com>
2022-08-02 19:42:06 +0530

    Add files via upload

---
+++ 2DArray.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int arr2d[3][3]= {{10,20,30},
+                      { 4, 5, 6},
+                      {1,2,3}  };
+    cout<<"Here I am going to dimonstrate the 2D array in matrix format"<<endl;
+    
+    cout<<"{"<<"{"<<arr2d[0][0]<<","<<arr2d[0][1]<<","<<arr2d[0][2]<<"}"<<","<<endl
+             <<"{"<<arr2d[1][0]<<","<<arr2d[1][1]<<","<<arr2d[1][2]<<"}"<<","<<endl
+             <<"{"<<arr2d[2][0]<<","<<arr2d[2][1]<<","<<arr2d[2][2]<<"}"<<"}"<<endl;
+    
+    
+    // for (int i = 0; i < 3; i++)
+    // {
+    //     for (int j = 0; j < 3; j++)
+    //     {
+            
+    //     }
+        
+    // }
+                                     
+    return 0;
+}
